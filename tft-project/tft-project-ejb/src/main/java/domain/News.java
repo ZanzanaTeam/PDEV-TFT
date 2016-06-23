5248556387d9549bbaa42b4b556c6796e385cf87
Samih KADRI <samih.kadri@gmail.com>
2016-06-23 02:14:58 +0200

    Création Entité Article Création page interne News
    Signed-off-by: Samih KADRI <samih.kadri@gmail.com>

---
+++ tft-project/tft-project-ejb/src/main/java/domain/News.java
@@ -0,0 +1,82 @@
+package domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import enumeration.ArticleStatus;
+
+@Entity
+public class News implements Serializable  {
+
+	/**
+	 * Author Samih  
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id; 
+	private String title;
+	private String body;
+	private Date publishDate;
+	private ArticleStatus articleStatus;
+	private String thumbnail;
+	private String summary;
+	
+	@Id
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	@Column(columnDefinition="text")
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public ArticleStatus getArticleStatus() {
+		return articleStatus;
+	}
+	public void setArticleStatus(ArticleStatus articleStatus) {
+		this.articleStatus = articleStatus;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public Date getPublishDate() {
+		return publishDate;
+	}
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", body=" + body + ", publishDate=" + publishDate
+				+ ", articleStatus=" + articleStatus + ", thumbnail=" + thumbnail + ", summary=" + summary + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
